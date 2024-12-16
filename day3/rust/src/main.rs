f8367f9ef82860239313aef0ed5d8c1d9930fab9
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2024-12-16 13:28:34 +0300

    rust: Day 3 - Part 2

--- day3/rust/src/main.rs
+++ day3/rust/src/main.rs
@@ -1,10 +1,12 @@
 use std::fs::read_to_string;
+
 fn main() {
-    println!("RESULT: {}", part_one("../input.txt"));
+    let input = read_to_string("../input.txt").unwrap();
+    println!("FIRST_RESULT: {}", part_one(&input));
+    println!("SECOND_RESULT: {}", part_two(&input));
 }
 
-fn part_one(path: &str) -> u32 {
-    let input = read_to_string(path).unwrap();
+fn part_one(input: &str) -> u32 {
     input
         .match_indices("mul(")
         .map(|(match_index, _)| {
@@ -38,12 +40,66 @@ fn part_one(path: &str) -> u32 {
         .sum()
 }
 
+fn part_two(input: &str) -> u32 {
+    let mut result: u32 = 0;
+
+    let mut token = arrayvec::ArrayString::<11>::new();
+    let mut skip = false;
+
+    for line in input.lines() {
+        for ch in line.chars() {
+            match ch {
+                'm' => token.push(ch),
+                'u' if token == *"m" => token.push(ch),
+                'l' if token == *"mu" => token.push(ch),
+                '(' if token == *"mul" => token.push(ch),
+                d if d.is_ascii_digit() && token.starts_with("mul(") => token.push(d),
+                ',' if token.starts_with("mul(")
+                    && token.chars().skip(4).all(|c| c.is_ascii_digit()) =>
+                {
+                    token.push(ch);
+                }
+                ')' if token.starts_with("mul(") && !skip => {
+                    if let Some((a, b)) = token[4..].split_once(",") {
+                        result += a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap();
+                    }
+                    token.clear();
+                }
+                'd' => token.push(ch),
+                'o' if token == *"d" => {
+                    token.push(ch);
+                    skip = false;
+                }
+                'n' if token == *"do" => token.push(ch),
+                '\'' if token == *"don" => token.push(ch),
+                't' if token == *"don'" => {
+                    token.clear();
+                    skip = true;
+                }
+                _ => token.clear(),
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn part1() {
-        assert_eq!(161, part_one("../input-test.txt"));
+        assert_eq!(
+            161,
+            part_one("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            48,
+            part_two("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+        )
     }
 }
